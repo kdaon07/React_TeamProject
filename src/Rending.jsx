dbbdc4d21bce2b646c75ae71698da347e789b998
김다온 <kdaon07@gmail.com>
2024-06-30 00:00:51 +0900

    test

--- src/Rending.jsx
+++ src/Rending.jsx
@@ -10,11 +10,11 @@ const Rending = () => {
 
   return (
     <div>
+      <div className='aside1'></div>
       <div className='box'>
         <button className='setBtn' onClick={() => console.log(value)}>세팅</button><br />
         <Cal value={value} onChange={onChange} />
       </div>
-      <div className='aside1'></div>
       <div className='aside2'></div>
     </div>
   );
