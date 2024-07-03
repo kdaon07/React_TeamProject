113ccd0fcdec235b71fe50a1b703e164efb923fd
김다온 <128460925+kdaon07@users.noreply.github.com>
2024-07-03 21:27:13 +0900

    commit

---
+++ src/Check.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import moment from 'moment';
+import 'react-calendar/dist/Calendar.css'
+import Cal from './Calendar';
+import './style.css';
+
+const Rending = () => {
+  return (
+    <div>
+      <div className='box'>
+      </div>
+    </div>
+  );
+};
+
+export default Rending;
